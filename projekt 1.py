2ac9449d2d17c026c0c31fdccd0fd64cb71fdfdd
Małgorzata Łabuzek <146942844+mallab20@users.noreply.github.com>
2024-04-28 17:15:36 +0200

    Update projekt 1.py

--- projekt 1.py
+++ projekt 1.py
@@ -2,10 +2,10 @@ produkt = 'gra'
 elementy = ['20 kart', 'pudełko']
 
 #określenie startowej dostępności
-#poziom0
+#poziom 0
 dostepnosc_gra = int(input("Początkowa dostępność gry (w sztukach): "))
-dostepnosc_opakowanie = int(input("Początkowa dostępność opakowań (w sztukach): "))
 #poziom1
+dostepnosc_opakowanie = int(input("Początkowa dostępność opakowań (w sztukach): "))
 dostepnosc_zestaw_kart = int(input("Początkowa dostępność zestawów kart (w sztukach): "))
 #poziom2
 dostepnosc_karton = int(input("Początkowa dostępność kartonów (w sztukach): "))
